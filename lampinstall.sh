8adba104fff6cd11a8c3e7a8e2a8cc17c3ebded2
arvage <arvage@yahoo.com>
2018-03-08 11:52:35 -0800

    adding info when all services are installed and running

--- lampinstall.sh
+++ lampinstall.sh
@@ -98,6 +98,10 @@ then
         fi
     fi
 else
-clear
+clear; echo -e "${PURPLE}LAMP Installer Script Created by Armin.G 2018\n***** NOTE: This script needs to run under root permission *****${NC}\n\n"
+echo -e "System Info:"
+echo -e "${GREEN}CPU: $CPU"
+echo -e "${GREEN}Machine IP: $IP"
+echo -e "Hostname: $HOST ${NC}\n"
 echo -e "\n${GREEN}All required applications/services are installed and running\n\n${NC}"
 fi
